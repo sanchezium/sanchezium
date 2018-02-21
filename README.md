1da70676dc86131a1480cce4a61a618aadd33fae
rick <rick@sanchezium.com>
2018-02-21 10:19:33 +0300

    commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sanchezium
+Wubba Lubba Dub Dub!
+
+Sanchezium is a POS-based Rick's interdimensional coin.
+
+Download compiled wallets: http://sanchezium.com/#download
+
+Compiled daemon for Ubuntu 14.04 LTS is included in the repository.
+
+Port: 11453 RPC Port: 11454
+
+See INSTALL for instructions on installing and running Sanchezium daemon and wallets.
+
+See doc/readme-qt.rst for instructions on building Sanchezium QT, the intended-for-end-users, nice-graphical-interface, reference implementation of Sanchezium (you most probably don' need this).
+
+See doc/build-*.txt for instructions on building Sancheziumd, the intended-for-services, no-graphical-interface, reference implementation of Sanchezium (you most probably don' need this).
---
+++ src/checkpoints.cpp
@@ -0,0 +1,93 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "txdb.h"
+#include "main.h"
+#include "uint256.h"
+
+
+static const int nCheckpointSpan = 5000;
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+		(0, uint256("0x00002409d045bd410fa3818912edcf228284370995795167bce6d53b2eb1c0cf"))
+		(3, uint256("0x487cdcbadbab6ca987ef5b8c3172ecf13bf9ed21bad6ad8489bb6e1c2a29d7ca"))
+		(5, uint256("0xc233cd995670fb6c064e93c033f326707a3d00afe6e6d7ee9658d2bd050b622d"))
+		(16, uint256("0x3f37d5e514c06798197eed45c4b33702e91d0bff2708f074566c7e2d6eef5e51"))
+		(25, uint256("0x0820ed53b5ef9deb14fc799cf4e4c5ce69ecc3ec0dec6f2642c3b19095525293"))
+		(29, uint256("0xc9a53c06faf65415fae8aad820d3ec3f6948633b2238728d2c683ef644eb246c"))
+		(32, uint256("0x300a2cc784bacd6b78d7b055dcd7866753fc5253fda170394005fa7ecd97028b"))
+    ;
+
+    // TestNet has no checkpoints
+    static MapCheckpoints mapCheckpointsTestnet;
+
+    bool CheckHardened(int nHeight, const uint256& hash)
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
+        if (i == checkpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        if (checkpoints.empty())
+            return 0;
+        return checkpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        MapCheckpoints& checkpoints = (TestNet() ? mapCheckpointsTestnet : mapCheckpoints);
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, checkpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+
+    // Automatically select a suitable sync-checkpoint
+    const CBlockIndex* AutoSelectSyncCheckpoint()
+    {
+        const CBlockIndex *pindex = pindexBest;
+        // Search backward for a block within max span and maturity window
+        while (pindex->pprev && pindex->nHeight + nCheckpointSpan > pindexBest->nHeight)
+            pindex = pindex->pprev;
+        return pindex;
+    }
+
+    // Check against synchronized checkpoint
+    bool CheckSync(int nHeight)
+    {
+        const CBlockIndex* pindexSync = AutoSelectSyncCheckpoint();
+        if (nHeight <= pindexSync->nHeight){
+            return false;
+        }
+        return true;
+    }
+}
